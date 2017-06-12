747cc66b351119c42c91815c3d64ff91ba98ec3a
shprink <julien.renaux@gmail.com>
2017-06-13 00:40:31 +0200

    fix purifycss script and gain 20% on CSS size

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,21 +2,30 @@
 PATH=$PATH:$(npm bin)
 set -x
 
+BUILDFOLDER=build/
+
 # clean up previous build
-rm -fr www/
+rm -fr $BUILDFOLDER
 
 # Prod build
-ionic-app-scripts build --prod
+ionic-app-scripts build --prod \
+                        --wwwDir $BUILDFOLDER
 
-# remove unused css (not perfect)
-# purifycss www/build/main.css www/build/*.main.js --min --out www/build/main.css
+# remove unused css (~20% gain)
+purifycss $BUILDFOLDER"build/main.css" \
+          $BUILDFOLDER"build/main.js" \
+          $BUILDFOLDER"build/*.main.js" \
+          --info \
+          --min \
+          --out $BUILDFOLDER"build/main.css" \
+          --whitelist ion-backdrop .bar-button-default ion-icon
 
 # ngu-app-shell --module src/app/app.module.ts
 
 # Generate our SW manifest
-ngu-sw-manifest --out www/ngsw-manifest.json \
-                --dist www/
+ngu-sw-manifest --out $BUILDFOLDER"ngsw-manifest.json" \
+                --dist $BUILDFOLDER
                 # --module src/app/app.module.ts \
 
 # Copy basic SW file
-cp node_modules/@angular/service-worker/bundles/worker-basic.js www/
+cp node_modules/@angular/service-worker/bundles/worker-basic.js $BUILDFOLDER
