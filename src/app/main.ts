22585330f4987bb0941d20df8a2105e6dc88e594
shprink <julien.renaux@gmail.com>
2017-07-24 13:55:09 +0200

    Add Toast to update SW

--- src/app/main.ts
+++ src/app/main.ts
@@ -9,14 +9,15 @@ platformBrowserDynamic()
         if ('serviceWorker' in navigator) {
             navigator.serviceWorker.register('worker-basic.js')
                 .then((reg) => {
+                    console.log('SW reg', reg);
                     if (reg.installing) {
-                        console.log('service worker installing');
+                        console.log('SW installing');
                     } else if (reg.waiting) {
-                        console.log('service worker installed');
+                        console.log('SW installed');
                     } else if (reg.active) {
-                        console.log('service worker active');
+                        console.log('SW active');
                     }
                 })
-                .catch(err => console.error('Service worker error', err));
+                .catch(err => console.error('SW error', err));
         }
     });
--- src/providers/service-worker/service-worker.ts
+++ src/providers/service-worker/service-worker.ts
@@ -13,30 +13,26 @@ export class ServiceWorkerProvider {
   constructor(
     public sw: NgServiceWorker,
     public toastCtrl: ToastController,
-  ) {
-    console.log('Hello ServiceWorkerProvider Provider');
-  }
+  ) {}
 
   run() {
-    this.sw.log().subscribe(logs => console.log('service-worker logs', logs));
+    this.sw.log().subscribe(logs => console.log('SW logs', logs));
     this.sw.updates.subscribe(res => {
-      console.log('SW updates', res, res.version);
-      switch (res.type) {
-        case 'activation':
-          let toast = this.toastCtrl.create({
-            message: 'A new version is available, reload ',
-            duration: 5000,
-            closeButtonText: "Reload",
-            showCloseButton: true,
-          });
-          toast.onDidDismiss((data, role) => role === 'close' && location.reload());
-          toast.present();
-          break;
-
-        default:
-          break;
+      if (res.type === 'pending') {
+        let toast = this.toastCtrl.create({
+          message: 'A new version is available, reload ',
+          closeButtonText: "Reload",
+          showCloseButton: true,
+        });
+        toast.onDidDismiss((data, role) => {
+          if (role === 'close') {
+            this.sw.activateUpdate(res.version)
+              .subscribe(value => value && location.reload());
+          };
+        });
+        toast.present();
       }
-      console.log('service-worker updates', res);
     });
+    this.sw.checkForUpdate();
   }
 }
